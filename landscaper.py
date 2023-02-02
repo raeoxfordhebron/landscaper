b3a222f8901e3a3f93af4eb2aa9732ab84479ebd
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-01 21:28:39 -0500

    landscaper: user can use teeth to cut grass

--- landscaper.py
+++ landscaper.py
@@ -1,3 +1,11 @@
 ## Game State
-game = {"teeth": 0, "money": 0}
+game = {"tool": 0, "money": 0}
 
+tools = [
+    {"name": "teeth", "profit": 1, "cost": 0 }
+]
+
+def cut_grass():
+    tool = tools[game["tool"]]
+    print(f"You cut the grass with {tool['name']} and make {tool['profit']}")
+    game["money"] += tool["profit"]
